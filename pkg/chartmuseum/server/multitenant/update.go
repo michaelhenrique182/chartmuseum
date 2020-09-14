2f93d89714108d3c61b10c27f9331c86b8c0db51
jdolitsky <393494+jdolitsky@users.noreply.github.com>
2020-09-13 23:34:55 -0400

    Add fixes for cache
    Signed-off-by: jdolitsky <393494+jdolitsky@users.noreply.github.com>

--- pkg/chartmuseum/server/multitenant/update.go
+++ pkg/chartmuseum/server/multitenant/update.go
@@ -1,99 +1,117 @@
+/*
+Copyright The Helm Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package multitenant
 
 import (
+	"time"
+
+	cm_logger "helm.sh/chartmuseum/pkg/chartmuseum/logger"
+
 	cm_storage "github.com/chartmuseum/storage"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	cm_logger "helm.sh/chartmuseum/pkg/chartmuseum/logger"
 	"helm.sh/helm/v3/pkg/repo"
-	"time"
 )
 
 type (
-	updateObject struct {
+	event struct {
 		RepoName     string             `json:"repo_name"`
-		OpType       OperationType      `json:"operation_type"`
+		OpType       operationType      `json:"operation_type"`
 		ChartVersion *repo.ChartVersion `json:"chart_version"`
 	}
 )
-type OperationType int
+type operationType int
 
 const (
-	UpdateChart OperationType = 0
-	AddChart    OperationType = 1
-	DeleteChart OperationType = 2
+	updateChart operationType = 0
+	addChart    operationType = 1
+	deleteChart operationType = 2
 )
 
-var (
-	updateObjectChan chan updateObject
-)
-
-func (server *MultiTenantServer) InitCacheTimer() {
-	// consume
-	InitUpdateObjectChan()
-	go func() {
-		server.Consumer()
-	}()
-
-	// delta update the cache every 5min (in case the files on the disk are manually manipulated)
-	go func() {
-		t := time.NewTicker(server.CacheInterval)
-		for _ = range t.C {
-			server.RebuildIndex()
-		}
-	}()
+func (server *MultiTenantServer) initCacheTimer() {
+	if server.CacheInterval > 0 {
+		// delta update the cache every X duration
+		// (in case the files on the disk are manually manipulated)
+		go func() {
+			t := time.NewTicker(server.CacheInterval)
+			for _ = range t.C {
+				server.RebuildIndex()
+			}
+		}()
+	} //else {
+	//	server.RebuildIndex()
+	//}
 }
 
-func InitUpdateObjectChan() {
-	updateObjectChan = make(chan updateObject, 100)
-	return
-}
-
-func (server *MultiTenantServer) Producer(repo string, operationType OperationType, chart *repo.ChartVersion) {
-	uo := updateObject{
+func (server *MultiTenantServer) emitEvent(repo string, operationType operationType, chart *repo.ChartVersion) {
+	e := event{
 		RepoName:     repo,
 		OpType:       operationType,
 		ChartVersion: chart,
 	}
-
-	updateObjectChan <- uo
+	server.EventChan <- e
 	log := server.Logger.ContextLoggingFn(&gin.Context{})
-	log(cm_logger.InfoLevel, "send update", zap.Any("object", uo))
+	log(cm_logger.DebugLevel, "event emitted", zap.Any("event", e))
 }
 
-func (server *MultiTenantServer) Consumer() {
-	server.Router.Logger.Info("start consumer")
+func (server *MultiTenantServer) receiveEvents() {
+	server.Router.Logger.Debug("start receiver")
 	for {
 		log := server.Logger.ContextLoggingFn(&gin.Context{})
-		uo := <-updateObjectChan
-		repo := uo.RepoName
-		log(cm_logger.InfoLevel, "receive update", zap.String("repo", repo), zap.Any("uo", uo))
-		tenant := server.Tenants[uo.RepoName]
-		tenant.RegenerationLock.Lock()
+		e := <-server.EventChan
+
+		repo := e.RepoName
+		log(cm_logger.DebugLevel, "event received", zap.Any("event", e))
 
 		entry, err := server.initCacheEntry(log, repo)
 		if err != nil {
 			log(cm_logger.ErrorLevel, "initCacheEntry fail", zap.Error(err), zap.String("repo", repo))
 			continue
 		}
-
 		index := entry.RepoIndex
-		switch uo.OpType {
-		case UpdateChart:
-			index.UpdateEntry(uo.ChartVersion)
-		case AddChart:
-			index.AddEntry(uo.ChartVersion)
-		case DeleteChart:
-			index.RemoveEntry(uo.ChartVersion)
+
+		tenant := server.Tenants[e.RepoName]
+		tenant.RegenerationLock.Lock()
+
+		if e.ChartVersion == nil {
+			log(cm_logger.WarnLevel, "event does not contain chart version", zap.String("repo", repo),
+				"operation_type", e.OpType)
+			tenant.RegenerationLock.Unlock()
+			continue
+		}
+
+		switch e.OpType {
+		case updateChart:
+			index.UpdateEntry(e.ChartVersion)
+		case addChart:
+			index.AddEntry(e.ChartVersion)
+		case deleteChart:
+			index.RemoveEntry(e.ChartVersion)
 		default:
 			log(cm_logger.ErrorLevel, "invalid operation type", zap.String("repo", repo),
-				"operation_type", uo.OpType)
+				"operation_type", e.OpType)
+			tenant.RegenerationLock.Unlock()
 			continue
 		}
 
 		err = index.Regenerate()
 		if err != nil {
 			log(cm_logger.ErrorLevel, "regenerate fail", zap.Error(err), zap.String("repo", repo))
+			tenant.RegenerationLock.Unlock()
 			continue
 		}
 		entry.RepoIndex = index
@@ -101,17 +119,18 @@ func (server *MultiTenantServer) Consumer() {
 		err = server.saveCacheEntry(log, entry)
 		if err != nil {
 			log(cm_logger.ErrorLevel, "saveCacheEntry fail", zap.Error(err), zap.String("repo", repo))
+			tenant.RegenerationLock.Unlock()
 			continue
 		}
 
 		if server.UseStatefiles {
 			// Dont wait, save index-cache.yaml to storage in the background.
 			// It is not crucial if this does not succeed, we will just log any errors
-			go server.saveStatefile(log, uo.RepoName, entry.RepoIndex.Raw)
+			go server.saveStatefile(log, e.RepoName, entry.RepoIndex.Raw)
 		}
 
 		tenant.RegenerationLock.Unlock()
-		log(cm_logger.InfoLevel, "success update", zap.String("repo", repo), zap.Any("uo", uo))
+		log(cm_logger.DebugLevel, "event handled successfully", zap.Any("event", e))
 	}
 }
 
